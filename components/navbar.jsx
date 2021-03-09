6989a855c8eb4cd8a802b1c166dcd8c0d57acf30
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-03-09 00:06:18 -0600

    Initial file setup

---
+++ components/navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+    <nav class="navbar" class="navbar navbar-dark bg-light">
+    <h1 id="myName">Gustaf G. Campoy</h1>
+    <ul class="nav navbar-right ml-auto">
+        {/* <li class="Contact">
+        <a class="nav-link" href="contact.html">Contact</a>
+        </li>  */}
+        <li class="Portfolio">
+            <a class="nav-link" href="portfolio.html">Portfolio</a>
+        </li>
+        <li class="About">
+            <a class="nav-link active" href="index.html">About</a>
+        </li>
+    </ul>
+</nav>
---
+++ components/portfolioBody.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+<div class="container">
+        <div class="jumbotron">
+            <div class="row">
+                <div class="col-md-8">
+                    <h1 id="Portfolio">Portfolio</h1>
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div><img class="img" class="img-fluid" width="400px" src="Assets/Images/NationalParks.jpg" class="mr-3" alt="NationalParks">
+                        <h5>National Park App</h5>
+                        <a class="nav-link" target="_blank" href="https://github.com/yermasog/Find-a-National-Park-app">GitHub</a>
+                        <a class="nav-link" target="_blank" href="https://yermasog.github.io/Find-a-National-Park-app/">Try It! </a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div><img class="img" class="img-fluid" width="400px" src="Assets/Images/passwordGen.jpg" class="mr-3" alt="passwordGen">
+                        <h5>PassWord Generator</h5>
+                        <a class="nav-link" target="_blank" href="https://github.com/Gustaf-987/Homework3-Button-Generator">GitHub</a>
+                        <a class="nav-link" target="_blank" href="https://gustaf-987.github.io/Homework3-Button-Generator/">Try It! </a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div><img class="img" class="img-fluid" width="400px" src="Assets/Images/workDayScheduler.jpg" class="mr-3" alt="workdayScheduler">
+                        <h5>Work Day Scheduler</h5>
+                        <a class="nav-link" target="_blank" href="https://github.com/Gustaf-987/Homework5_CalendarApp">GitHub</a>
+                        <a class="nav-link" target="_blank" href=" https://gustaf-987.github.io/Homework5_CalendarApp/">Try It! </a>
+                    </div>
+                </div>
+            </div>
+            <!-- <div class="row">
+          <div class="col-md-4">
+            <div><img class="img" class="img-fluid" width="400px" src="20201214_091452.jpg" class="mr-3" alt="me"></div>
+          </div>
+          <div class="col-md-4">
+            <div><img class="img" class="img-fluid" width="400px" src="20201214_091452.jpg" class="mr-3" alt="me"></div>
+          </div>
+          <div class="col-md-4">
+            <div><img class="img" class="img-fluid" width="400px" src="20201214_091452.jpg" class="mr-3" alt="me"></div>
+          </div>
+        </div> -->
+
+        </div>
+    </div>
