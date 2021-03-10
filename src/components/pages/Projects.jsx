9f7d52439b7a88dd3f29baf9b0ab1ba8c0eafaa8
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-03-09 22:13:02 -0600

    Setup Projects page

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -2,19 +2,34 @@ import React from "react";
 
 const Projects = () => (
   <div>
-    <h1>Blog Page</h1>
-    <p>
-      Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula. Curabitur pellentesque
-      turpis sit amet eros iaculis, a mollis arcu dictum. Ut vel ante eget massa ornare placerat.
-      Etiam nisl orci, finibus sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum
-      nunc, sed pretium risus rutrum eget. Nam consequat, ligula in faucibus vestibulum, nisi justo
-      laoreet risus, luctus luctus mi lacus sit amet libero. Class aptent taciti sociosqu ad litora
-      torquent per conubia nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-      lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec placerat
-      accumsan mi, ut congue neque placerat eu. Donec nec ipsum in velit pellentesque vehicula sit
-      amet at augue. Maecenas aliquam bibendum congue. Pellentesque semper, lectus non ullamcorper
-      iaculis, est ligula suscipit velit, sed bibendum turpis dui in sapien.
-    </p>
+    <h1>Portfolio Sample</h1>
+    <ul>
+      <li>
+      <img width="400px" src="../NationalParks.jpg" alt="NationalParks"></img>
+        <h5>National Park App</h5>
+        <a class="nav-link" target="_blank" href="https://github.com/yermasog/Find-a-National-Park-app">GitHub</a>
+        <a class="nav-link" target="_blank" href="https://yermasog.github.io/Find-a-National-Park-app/">Try It! </a>
+      </li>
+      <li>
+      <img width="400px" src="../passwordGen.jpg" alt="passwordGen"></img>
+        <h5>PassWord Generator</h5>
+        <a class="nav-link" target="_blank" href="https://github.com/Gustaf-987/Homework3-Button-Generator">GitHub</a>
+        <a class="nav-link" target="_blank" href="https://gustaf-987.github.io/Homework3-Button-Generator/">Try It! </a>
+      </li>
+      <li>
+      <img width="400px" src="../crimeAdeDemo.gif" alt="passwordGen"></img>
+        <h5>CrimeAde</h5>
+        <a class="nav-link" target="_blank" href="https://github.com/Gustaf-987/charmeleon">GitHub</a>
+        <a class="nav-link" target="_blank" href="https://infinite-savannah-89804.herokuapp.com/">Try It! </a>
+      </li>
+    </ul>
+    <li>
+    <img width="400px" src="../workDayScheduler.jpg" alt="workdayScheduler"></img>
+      <h5>Work Day Scheduler</h5>
+      <a class="nav-link" target="_blank" href="https://github.com/Gustaf-987/Homework5_CalendarApp">GitHub</a>
+      <a class="nav-link" target="_blank" href=" https://gustaf-987.github.io/Homework5_CalendarApp/">Try It! </a>
+    </li>
+
   </div>
 );
 
