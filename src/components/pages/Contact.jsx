43d72d41806db924be17173721da1b5330329db4
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-03-09 21:23:50 -0600

    Setup Contact page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -3,10 +3,14 @@ import React from "react";
 const Contact = () => (
   <div>
     <h1>Contact Page</h1>
-    <p>
-      
-    </p>
+    <h4>Follow me here:</h4>
+    <ul>
+      <li><a class="text-right" target="_blank" href="https://github.com/Gustaf-987">GitHub</a></li>
+      <li><a class="text-right" target="_blank" href="https://www.linkedin.com/in/gustaf-g-campoy-880bbb1b9/">Linkedin</a></li>
+      <li><a href="https://docs.google.com/document/d/1pCeshD0yI7UgjTZuB8Cp1tBHh_drU__66C27CJMMsrU/edit?usp=sharing">Resume</a></li>
+      </ul>
   </div>
+
 );
 
 export default Contact;
